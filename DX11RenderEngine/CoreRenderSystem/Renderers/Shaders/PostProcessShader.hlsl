4f6c217874a5d3ce6f17b078aecad511a877f77b
Alexandr <sashadz2002@gmail.com>
2022-04-08 15:12:20 +0300

    Add SSAO to PP

--- DX11RenderEngine/CoreRenderSystem/Renderers/Shaders/PostProcessShader.hlsl
+++ DX11RenderEngine/CoreRenderSystem/Renderers/Shaders/PostProcessShader.hlsl
@@ -86,7 +86,7 @@ float4 psIn(PSIn input) : SV_Target
 
     float4 color = diffuseColor.Sample(blureSampler, texCoord);
     float4 light = lightmap.Sample(blureSampler, texCoord);
-    light *= occlusion.Sample(pointSampler, texCoord);
+    light = light * occlusion.Sample(pointSampler, texCoord).xxxx;
     light.rgb += float3(ppCosntBuffer.lightAdd,ppCosntBuffer.lightAdd,ppCosntBuffer.lightAdd);
     float4 bloom = bloomMask.Sample(blureSampler, texCoord);
     
